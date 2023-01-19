879f3c48ff96584124589de5e5b5474a84c36f42
Shahidnsu <shahid.khan@northsouth.edu>
2023-01-19 11:30:08 +0600

    8 errors remaning --@ngModule annotation reaming and also from bulider error is gone

--- client/src/app/application-form/application-form.component.spec.ts
+++ client/src/app/application-form/application-form.component.spec.ts
@@ -2,13 +2,16 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ApplicationFormComponent } from './application-form.component';
 import { FormBuilder } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ApiClientService } from '../services/api-client.service';
 describe('ApplicationFormComponent', () => {
   let component: ApplicationFormComponent;
   let fixture: ComponentFixture<ApplicationFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormBuilder],
+      imports: [HttpClientTestingModule],
+      providers: [HttpClientTestingModule, FormBuilder, ApiClientService],
       declarations: [ApplicationFormComponent],
     }).compileComponents();
 
