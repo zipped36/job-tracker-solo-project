a7d89c569ee507f70313fa3da03ddb5eb32a1e36
Kh Shuhat Uddin <shuhat1971@gmail.com>
2023-01-19 08:46:40 +0600

    env added, db changed

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const DB_PORT = 27017
 const DB_NAME = "Job_Tracker"
-const uri = `mongodb://127.0.0.1:${DB_PORT}/${DB_NAME}`
+const uri = `mongodb+srv://shuhat:shuhat234@newcluster.adznrpo.mongodb.net/?retryWrites=true&w=majority`
 
 module.exports = { mongoose, uri };
